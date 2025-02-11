4a217a9e2e1d2d2e4d0930b501379ffd51b53901
Lukii <lukas.janecek.02@gmail.com>
2025-02-11 20:51:10 +0100

    First commit and first small changes

---
+++ JAN0837_DP/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace JAN0837_DP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            statusStripMain = new StatusStrip();
+            toolStripMain = new ToolStrip();
+            SuspendLayout();
+            // 
+            // statusStripMain
+            // 
+            statusStripMain.ImageScalingSize = new Size(20, 20);
+            statusStripMain.Location = new Point(0, 428);
+            statusStripMain.Name = "statusStripMain";
+            statusStripMain.Size = new Size(800, 22);
+            statusStripMain.TabIndex = 0;
+            statusStripMain.Text = "statusStrip1";
+            // 
+            // toolStripMain
+            // 
+            toolStripMain.ImageScalingSize = new Size(20, 20);
+            toolStripMain.Location = new Point(0, 0);
+            toolStripMain.Name = "toolStripMain";
+            toolStripMain.Size = new Size(800, 25);
+            toolStripMain.TabIndex = 1;
+            toolStripMain.Text = "toolStrip1";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(toolStripMain);
+            Controls.Add(statusStripMain);
+            Name = "MainForm";
+            Text = "Form1";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private StatusStrip statusStripMain;
+        private ToolStrip toolStripMain;
+    }
+}
---
+++ JAN0837_DP/MainForm.cs
@@ -0,0 +1,23 @@
+namespace JAN0837_DP
+{
+    public partial class MainForm : Form
+    {
+        // Threads 
+
+        // FLags 
+
+        // Queues 
+
+        // Lists 
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

4704d21773c4f619b46cd090969d6672a4bc785a
Lukii <lukas.janecek.02@gmail.com>
2025-09-12 14:36:49 +0200

    Crossroad data showed in crossroad page

--- JAN0837_DP/ReactFE/jan0837_reactfe/src/Pages/CrossroadPage.js
+++ JAN0837_DP/ReactFE/jan0837_reactfe/src/Pages/CrossroadPage.js
@@ -12,12 +12,30 @@ import Clock from '../Components/Clock.js';
 import PictureSwitcher from '../Components/PictureSwitcher.js';
 import ResponsiveImage from '../Components/ResponsiveImage.js';
 
+import { useRefresh } from '../Communication/RefreshContext.js';
+import { useData } from '../Communication/DataProvider';
+
 const names = ['crosswalk_ped_green_1920x1080_169', 'crosswalk_vehicle_yellow_1920x1080_169', 'crosswalk_vehicle_green_1920x1080_169', 'crosswalk_ped_green_800x600_43', 'crosswalk_vehicle_yellow_800x600_43', 'crosswalk_vehicle_green_800x600_43'];
 const ext = 'png';         
 const folder = 'images'; 
 
 function CrossroadParamsSidebar({names, idx, onPrev, onNext, onJump,}) 
 {
+  const { interval, setInterval } = useRefresh();
+  const { data, saveData, error, isFetching, refresh } = useData();
+
+  const bool = (() => {const t = String(data?.toggle ?? '').toLowerCase();return t === 'true' || t === 'on' || t === '1';})();
+  const trafficLight1_green = (() => {const t = String(data?.trafficLight1_green ?? '').toLowerCase();})();
+  const trafficLight1_yellow = Boolean(data?.trafficLight1_yellow ?? false);
+  const trafficLight1_red = Boolean(data?.trafficLight1_red ?? true); 
+  const trafficLight2_green = Boolean(data?.trafficLight2_green ?? false);
+  const trafficLight2_yellow = Boolean(data?.trafficLight2_yellow ?? false);
+  const trafficLight2_red = Boolean(data?.trafficLight2_red ?? true); 
+  const pedestrian1_green = Boolean(data?.pedestrian1_green ?? false);
+  const pedestrian1_red = Boolean(data?.pedestrian1_red ?? true); 
+  const pedestrian2_green = Boolean(data?.pedestrian2_green ?? false);
+  const pedestrian2_red = Boolean(data?.pedestrian2_red ?? true);
+
   return (
     <div className="p-3 border-start h-100">
       <div className="fw-semibold mb-3">Parametry</div>
@@ -46,18 +64,17 @@ function CrossroadParamsSidebar({names, idx, onPrev, onNext, onJump,})
         ))}
       </Form.Select>
 
-      <div className="d-grid gap-2">
-        <Button variant="primary">Start</Button>
-        <Button variant="outline-secondary">Stop</Button>
-        <Form.Group className="mt-3">
-          <Form.Label>Název</Form.Label>
-          <Form.Control type="text" placeholder="Zadej název…" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Poznámky</Form.Label>
-          <Form.Control as="textarea" rows={4} placeholder="…" />
-        </Form.Group>
-        <Button variant="success">Uložit</Button>
+      <div>
+        <div><strong>TL1_G:</strong>{trafficLight1_green}</div>
+        <div><strong>TL1_Y:</strong>{trafficLight1_yellow}</div>
+        <div><strong>TL1_R:</strong>{trafficLight1_red}</div>
+        <div><strong>TL2_G:</strong>{trafficLight2_green}</div>
+        <div><strong>TL2_Y:</strong>{trafficLight2_yellow}</div>
+        <div><strong>TL2_R:</strong>{trafficLight2_red}</div>
+        <div><strong>PL1_G:</strong>{pedestrian1_green}</div>
+        <div><strong>PL1_R:</strong>{pedestrian1_red}</div>
+        <div><strong>PL2_G:</strong>{pedestrian2_green}</div>
+        <div><strong>PL2_R:</strong>{pedestrian2_red}</div>
       </div>
     </div>
   );
@@ -81,6 +98,7 @@ function CrossroadPage({ setAside }) {
     preload(names[(idx + 1) % names.length]);
     preload(names[(idx - 1 + names.length) % names.length]);
   }, [idx]);
+
   {/*
   useEffect(()=>{
       setAside(
@@ -93,25 +111,27 @@ function CrossroadPage({ setAside }) {
       return ()=> setAside(null);
     }, [setAside]);
   */}
+
   return (
     <Row className="crossroadpage">
       <Col /*xs={12} lg={10}*/>
-        {/*}
+        
         <div className="mt-3">
           <Picture name={names[idx]} ext={ext} folder={folder} />
         </div>
-        */}
-
+        
         {/*}
-        <div style={{display:"grid", gap:12}}>
-          <ResponsiveImage name="/crosswalk_ped_green_1920x1080_169" alt="Schema křižovatky" aspect="16 / 9" fit="contain"/>
-        </div>
         */}
-        {/*}
+        
         <div style={{display:"grid", gap:12}}>
-          <ResponsiveImage name="/crosswalk_ped_green_800x600_43" alt="Schema křižovatky" aspect="4 / 3" fit="contain"/>
+          <ResponsiveImage name="/crosswalk_ped_green_1920x1080_169" alt="Schema křižovatky 16:9" aspect="16 / 9" fit="contain"/>
         </div>
-        */}
+        
+        
+        <div style={{display:"grid", gap:12}}>
+          <ResponsiveImage name="/crosswalk_ped_green_800x600_43" alt="Schema křižovatky 4:3" aspect="4 / 3" fit="contain"/>
+        </div>
+        
         
         <div>
           <PictureSwitcher names={names} ext="png" imgClassName="shadow-sm" />
